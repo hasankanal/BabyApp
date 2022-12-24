5c04cb2e233f02d8705dca3c8d0784e4f3b9e9a4
izelhatipoglu <izelhatipoglu@gmail.com>
2022-12-24 23:46:58 +0300

    update date

--- app/src/main/java/com/izelhatipoglu/babyapp/model/Home.kt
+++ app/src/main/java/com/izelhatipoglu/babyapp/model/Home.kt
@@ -1,11 +1,12 @@
 package com.izelhatipoglu.babyapp.model
 
 data class Home(
-    val month: String? = null,
+    var month: String? = null,
     val doctorAppointment: String? = null,
     val doctorName: String? = null,
-    val doctorNotes: String? = null,
+    var doctorNotes: String? = null,
     val photo: String? = null,
-    val userName: String? = null,
+    var userName: String? = null,
+    var name: String? = null,
     val type: String? = null
 )
